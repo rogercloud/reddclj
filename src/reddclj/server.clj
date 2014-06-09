dba2aba69dd23ee0452f0e419265bd847380c7c7
roger <roger@debian.debian>
2014-06-09 15:32:36 -0400

    add hashcode state saving

--- src/reddclj/server.clj
+++ src/reddclj/server.clj
@@ -1,8 +1,17 @@
 (ns reddclj.server
   (:require [ring.adapter.jetty :as jetty]))
 
+(def modhash (ref ""))
+(def hashcode (ref ""))
+
+(defn- store-hash [request]
+  (dosync 
+    (ref-set modhash "")
+    (ref-set hashcode "")))
+
 (defn- handler [request]
-  (do
+  (let []
+    (store-hash (:query-string request))
     {:status 200
      :headers {"Content-Type" "text/html"}
      :body (:query-string request)}))
